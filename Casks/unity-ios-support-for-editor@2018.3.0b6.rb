b2460c45dfc9a4bfafe6ed15196ba7ba5873f784
agent_2 <agent_2@185.74.13.31>
2018-10-18 10:54:00 +0100

    Update unity versions

---
+++ Casks/unity-ios-support-for-editor@2018.3.0b6.rb
@@ -0,0 +1,35 @@
+cask 'unity-ios-support-for-editor@2018.3.0b6' do
+  version '2018.3.0b6,f5aefbeed0ac'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/f5aefbeed0ac/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-2018.3.0b6.pkg"
+  name 'iOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-iOS-Support-for-Editor-2018.3.0b6.pkg'
+
+  depends_on cask: 'unity@2018.3.0b6'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2018.3.0b6"
+      FileUtils.move "/Applications/Unity-2018.3.0b6", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2018.3.0b6"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2018.3.0b6/PlaybackEngines/iOSSupport'
+end
