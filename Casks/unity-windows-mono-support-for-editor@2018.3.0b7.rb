933669bcbf66d78ae21b736b9af7f52c29884f08
agent_1 <agent_1@185.74.13.31>
2018-10-25 10:53:55 +0100

    Update unity versions

---
+++ Casks/unity-windows-mono-support-for-editor@2018.3.0b7.rb
@@ -0,0 +1,35 @@
+cask 'unity-windows-mono-support-for-editor@2018.3.0b7' do
+  version '2018.3.0b7,af029f4527e0'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/af029f4527e0/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2018.3.0b7.pkg"
+  name 'Windows Build Support (Mono)'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Windows-Mono-Support-for-Editor-2018.3.0b7.pkg'
+
+  depends_on cask: 'unity@2018.3.0b7'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2018.3.0b7"
+      FileUtils.move "/Applications/Unity-2018.3.0b7", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2018.3.0b7"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2018.3.0b7/PlaybackEngines/WindowsStandaloneSupport'
+end
--- README.md
+++ README.md
@@ -16,18 +16,18 @@ brew tap wooga/unityversions-beta
 Now you can install any version hosted as cask with
 
 ```bash
-brew cask install unity@2018.3.0b6
+brew cask install unity@2018.3.0b7
 ```
 
 -or-
 
 ```bash
-brew cask install unity-android-support-for-editor@2018.3.0b6
+brew cask install unity-android-support-for-editor@2018.3.0b7
 ```
 
 Unity will be install under `/Applications/Unity-$VERSION`. You can install multiple versions at the same time. All support packages will be installed in the parent Unity directory. Means:
 
-`unity-android-support-for-editor@2018.3.0b6` --> `/Applications/Unity-2018.3.0b6/PlaybackEngines/`
+`unity-android-support-for-editor@2018.3.0b7` --> `/Applications/Unity-2018.3.0b7/PlaybackEngines/`
 
 If you have a unity version installed under `/Applications/Unity/` it will be moved to
 `/Applications/Unity.temp` for the time of the installation.
@@ -98,6 +98,7 @@ Unity Versions available
 | 2018.3.0b4 |
 | 2018.3.0b5 |
 | 2018.3.0b6 |
+| 2018.3.0b7 |
 
 
 Adding a new Version
