a065b4cde0104e1b4a5e74bef2f49155f817901f
agent_1 <agent_1@185.74.13.53>
2018-11-24 11:54:35 +0100

    Update unity versions

---
+++ Casks/unity-vuforia-ar-support-for-editor@2019.1.0a10.rb
@@ -0,0 +1,35 @@
+cask 'unity-vuforia-ar-support-for-editor@2019.1.0a10' do
+  version '2019.1.0a10,84f9b5a9b69f'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/84f9b5a9b69f/MacEditorTargetInstaller/UnitySetup-Vuforia-AR-Support-for-Editor-2019.1.0a10.pkg"
+  name 'Vuforia Augmented Reality Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Vuforia-AR-Support-for-Editor-2019.1.0a10.pkg'
+
+  depends_on cask: 'unity@2019.1.0a10'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.1.0a10"
+      FileUtils.move "/Applications/Unity-2019.1.0a10", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.1.0a10"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.0a10/PlaybackEngines/VuforiaSupport'
+end
--- README.md
+++ README.md
@@ -16,18 +16,18 @@ brew tap wooga/unityversions-beta
 Now you can install any version hosted as cask with
 
 ```bash
-brew cask install unity@2018.3.0b11
+brew cask install unity@2019.1.0a10
 ```
 
 -or-
 
 ```bash
-brew cask install unity-android-support-for-editor@2018.3.0b11
+brew cask install unity-android-support-for-editor@2019.1.0a10
 ```
 
 Unity will be install under `/Applications/Unity-$VERSION`. You can install multiple versions at the same time. All support packages will be installed in the parent Unity directory. Means:
 
-`unity-android-support-for-editor@2018.3.0b11` --> `/Applications/Unity-2018.3.0b11/PlaybackEngines/`
+`unity-android-support-for-editor@2019.1.0a10` --> `/Applications/Unity-2019.1.0a10/PlaybackEngines/`
 
 If you have a unity version installed under `/Applications/Unity/` it will be moved to
 `/Applications/Unity.temp` for the time of the installation.
@@ -106,6 +106,7 @@ Unity Versions available
 | 2019.1.0a9 |
 | 2018.3.0b10 |
 | 2018.3.0b11 |
+| 2019.1.0a10 |
 
 
 Adding a new Version
