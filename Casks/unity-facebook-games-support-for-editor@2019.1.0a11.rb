2eaa3514d3adca69a4b211cc4a168d5fd6cbab6e
agent_2 <agent_2@185.74.13.53>
2018-12-06 11:55:34 +0100

    Update unity versions

---
+++ Casks/unity-facebook-games-support-for-editor@2019.1.0a11.rb
@@ -0,0 +1,35 @@
+cask 'unity-facebook-games-support-for-editor@2019.1.0a11' do
+  version '2019.1.0a11,0f106840e011'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/0f106840e011/MacEditorTargetInstaller/UnitySetup-Facebook-Games-Support-for-Editor-2019.1.0a11.pkg"
+  name 'Facebook Gameroom Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Facebook-Games-Support-for-Editor-2019.1.0a11.pkg'
+
+  depends_on cask: 'unity@2019.1.0a11'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.1.0a11"
+      FileUtils.move "/Applications/Unity-2019.1.0a11", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.1.0a11"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.0a11/PlaybackEngines/Facebook'
+end
--- README.md
+++ README.md
@@ -16,18 +16,18 @@ brew tap wooga/unityversions-beta
 Now you can install any version hosted as cask with
 
 ```bash
-brew cask install unity@2018.3.0b12
+brew cask install unity@2019.1.0a11
 ```
 
 -or-
 
 ```bash
-brew cask install unity-android-support-for-editor@2018.3.0b12
+brew cask install unity-android-support-for-editor@2019.1.0a11
 ```
 
 Unity will be install under `/Applications/Unity-$VERSION`. You can install multiple versions at the same time. All support packages will be installed in the parent Unity directory. Means:
 
-`unity-android-support-for-editor@2018.3.0b12` --> `/Applications/Unity-2018.3.0b12/PlaybackEngines/`
+`unity-android-support-for-editor@2019.1.0a11` --> `/Applications/Unity-2019.1.0a11/PlaybackEngines/`
 
 If you have a unity version installed under `/Applications/Unity/` it will be moved to
 `/Applications/Unity.temp` for the time of the installation.
@@ -108,6 +108,7 @@ Unity Versions available
 | 2018.3.0b11 |
 | 2019.1.0a10 |
 | 2018.3.0b12 |
+| 2019.1.0a11 |
 
 
 Adding a new Version
