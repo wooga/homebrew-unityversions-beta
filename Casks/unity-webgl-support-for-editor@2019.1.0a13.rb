52254bf870523a8abf554ded8dfd6311fe78fb1e
agent_2 <agent_2@185.74.13.66>
2019-01-04 11:53:29 +0100

    Update unity versions

---
+++ Casks/unity-webgl-support-for-editor@2019.1.0a13.rb
@@ -0,0 +1,35 @@
+cask 'unity-webgl-support-for-editor@2019.1.0a13' do
+  version '2019.1.0a13,3de2277bb0e6'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/3de2277bb0e6/MacEditorTargetInstaller/UnitySetup-WebGL-Support-for-Editor-2019.1.0a13.pkg"
+  name 'WebGL Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-WebGL-Support-for-Editor-2019.1.0a13.pkg'
+
+  depends_on cask: 'unity@2019.1.0a13'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.1.0a13"
+      FileUtils.move "/Applications/Unity-2019.1.0a13", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.1.0a13"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.0a13/PlaybackEngines/WebGLSupport'
+end
--- README.md
+++ README.md
@@ -16,18 +16,18 @@ brew tap wooga/unityversions-beta
 Now you can install any version hosted as cask with
 
 ```bash
-brew cask install unity@2019.1.0a12
+brew cask install unity@2019.1.0a13
 ```
 
 -or-
 
 ```bash
-brew cask install unity-android-support-for-editor@2019.1.0a12
+brew cask install unity-android-support-for-editor@2019.1.0a13
 ```
 
 Unity will be install under `/Applications/Unity-$VERSION`. You can install multiple versions at the same time. All support packages will be installed in the parent Unity directory. Means:
 
-`unity-android-support-for-editor@2019.1.0a12` --> `/Applications/Unity-2019.1.0a12/PlaybackEngines/`
+`unity-android-support-for-editor@2019.1.0a13` --> `/Applications/Unity-2019.1.0a13/PlaybackEngines/`
 
 If you have a unity version installed under `/Applications/Unity/` it will be moved to
 `/Applications/Unity.temp` for the time of the installation.
@@ -111,6 +111,7 @@ Unity Versions available
 | 2019.1.0a11 |
 | 2018.3.0f1 |
 | 2019.1.0a12 |
+| 2019.1.0a13 |
 
 
 Adding a new Version
