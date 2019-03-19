19b587a79dfb37a55ada0101b760c9b4a07b20c0
agent_1 <agent_1@185.74.13.53>
2019-03-19 11:54:55 +0100

    Update unity versions

---
+++ Casks/unity-linux-support-for-editor@2019.1.0b8.rb
@@ -0,0 +1,35 @@
+cask 'unity-linux-support-for-editor@2019.1.0b8' do
+  version '2019.1.0b8,0862b00eb5a3'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/0862b00eb5a3/MacEditorTargetInstaller/UnitySetup-Linux-Support-for-Editor-2019.1.0b8.pkg"
+  name 'Linux Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Linux-Support-for-Editor-2019.1.0b8.pkg'
+
+  depends_on cask: 'unity@2019.1.0b8'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.1.0b8"
+      FileUtils.move "/Applications/Unity-2019.1.0b8", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.1.0b8"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.0b8/PlaybackEngines/LinuxStandaloneSupport'
+end
