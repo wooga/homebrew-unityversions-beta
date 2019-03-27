2ac557e015515c76893cc1fa3d378afa3a5c70b8
agent_1 <agent_1@185.74.13.53>
2019-03-27 11:55:21 +0100

    Update unity versions

---
+++ Casks/unity@2019.1.0b9.rb
@@ -0,0 +1,29 @@
+cask 'unity@2019.1.0b9' do
+  version '2019.1.0b9,f1481f10a935'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/f1481f10a935/MacEditorInstaller/Unity.pkg"
+  name 'Unity 2019.1.0b9'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Unity.pkg'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+  end
+
+  postflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity-2019.1.0b9"
+    end
+
+    if File.exist? "/Applications/Unity.temp"
+        FileUtils.move "/Applications/Unity.temp", "/Applications/Unity"
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.1.0b9'
+end
