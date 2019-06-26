3193c6851cb4ab521906dde19e20b547c1d0d117
agent_1 <agent_1@185.74.13.53>
2019-06-26 11:55:12 +0200

    Update unity versions

---
+++ Casks/unity-mac-il2cpp-support-for-editor@2019.2.0b7.rb
@@ -0,0 +1,35 @@
+cask 'unity-mac-il2cpp-support-for-editor@2019.2.0b7' do
+  version '2019.2.0b7,87c9ecb96495'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/87c9ecb96495/MacEditorTargetInstaller/UnitySetup-Mac-IL2CPP-Support-for-Editor-2019.2.0b7.pkg"
+  name 'Mac Build Support (IL2CPP)'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Mac-IL2CPP-Support-for-Editor-2019.2.0b7.pkg'
+
+  depends_on cask: 'unity@2019.2.0b7'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2019.2.0b7"
+      FileUtils.move "/Applications/Unity-2019.2.0b7", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2019.2.0b7"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2019.2.0b7/Unity.app/Contents/PlaybackEngines/MacStandaloneSupport'
+end
