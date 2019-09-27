baa71edea79cc0d4df43e0b123581d3052192d0e
agent_1 <agent_1@10.41.110.23>
2019-09-27 11:55:33 +0200

    Update unity versions

---
+++ Casks/unity-ios-support-for-editor@2020.1.0a5.rb
@@ -0,0 +1,35 @@
+cask 'unity-ios-support-for-editor@2020.1.0a5' do
+  version '2020.1.0a5,8af4c0145d29'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/8af4c0145d29/MacEditorTargetInstaller/UnitySetup-iOS-Support-for-Editor-2020.1.0a5.pkg"
+  name 'iOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-iOS-Support-for-Editor-2020.1.0a5.pkg'
+
+  depends_on cask: 'unity@2020.1.0a5'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2020.1.0a5"
+      FileUtils.move "/Applications/Unity-2020.1.0a5", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2020.1.0a5"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2020.1.0a5/PlaybackEngines/iOSSupport'
+end
