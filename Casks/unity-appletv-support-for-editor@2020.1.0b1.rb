4e48f037f91788aabcb84dc038f8ac7a282f490c
agent_1 <agent_1@10.41.110.23>
2020-03-12 11:55:32 +0100

    Update unity versions

---
+++ Casks/unity-appletv-support-for-editor@2020.1.0b1.rb
@@ -0,0 +1,35 @@
+cask 'unity-appletv-support-for-editor@2020.1.0b1' do
+  version '2020.1.0b1,92a30bddf81d'
+  sha256 :no_check
+
+  url "http://beta.unity3d.com/download/92a30bddf81d/MacEditorTargetInstaller/UnitySetup-AppleTV-Support-for-Editor-2020.1.0b1.pkg"
+  name 'tvOS Build Support'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-AppleTV-Support-for-Editor-2020.1.0b1.pkg'
+
+  depends_on cask: 'unity@2020.1.0b1'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2020.1.0b1"
+      FileUtils.move "/Applications/Unity-2020.1.0b1", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2020.1.0b1"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2020.1.0b1/PlaybackEngines/AppleTVSupport'
+end
