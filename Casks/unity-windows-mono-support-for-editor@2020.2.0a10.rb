99f5f879ca326d9c4b1c2c8d95826b49b4e3be0c
agent_1 <agent_1@10.41.110.23>
2020-05-07 11:55:06 +0200

    Update unity versions

---
+++ Casks/unity-windows-mono-support-for-editor@2020.2.0a10.rb
@@ -0,0 +1,35 @@
+cask 'unity-windows-mono-support-for-editor@2020.2.0a10' do
+  version '2020.2.0a10,11fb376d50c9'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/11fb376d50c9/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2020.2.0a10.pkg"
+  name 'Windows Build Support (Mono)'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'UnitySetup-Windows-Mono-Support-for-Editor-2020.2.0a10.pkg'
+
+  depends_on cask: 'unity@2020.2.0a10'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+      FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+
+    if File.exist? "/Applications/Unity-2020.2.0a10"
+      FileUtils.move "/Applications/Unity-2020.2.0a10", '/Applications/Unity'
+    end
+  end
+
+  postflight do
+    if File.exist? '/Applications/Unity'
+      FileUtils.move '/Applications/Unity', "/Applications/Unity-2020.2.0a10"
+    end
+
+    if File.exist? '/Applications/Unity.temp'
+      FileUtils.move '/Applications/Unity.temp', '/Applications/Unity'
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-2020.2.0a10/PlaybackEngines/WindowsStandaloneSupport'
+end
